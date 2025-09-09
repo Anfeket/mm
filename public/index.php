3ec6daba14fa05b065b9a1bfaa72ba751ea81ab7
anfeket <anfeket@gmail.com>
2025-09-09 23:34:31 +0200

    Should work 2

--- public/index.php
+++ public/index.php
@@ -8,22 +8,22 @@ $path = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 // simple routing
 if ($path === '' || $path === 'posts') {
     // list of posts
-    require_once __DIR__ . '/../src/controllers/PostsController.php';
+    require_once __DIR__ . '/../controllers/PostsController.php';
     PostsController::index();
 }
 elseif (preg_match('#^post/(\d+)$#', $path, $m)) {
     // show single post
-    require_once __DIR__ . '/../src/controllers/PostsController.php';
+    require_once __DIR__ . '/../controllers/PostsController.php';
     PostsController::show((int)$m[1]);
 }
 elseif ($path === 'tags') {
-    require_once __DIR__ . '/../src/controllers/TagsController.php';
+    require_once __DIR__ . '/../controllers/TagsController.php';
     TagsController::index();
 }
 else {
     http_response_code(404);
     $title = "404 – mm";
-    include __DIR__ . '/../src/views/layout/head.php';
+    include __DIR__ . '/../views/layout/head.php';
     echo "<main><h1>404 Not Found</h1></main>";
-    include __DIR__ . '/../src/views/layout/footer.php';
+    include __DIR__ . '/../views/layout/footer.php';
 }
