982032e0d100d867ad2ed127d76bafaf77d83b0c
anfeket <anfeket@gmail.com>
2025-10-12 16:50:51 +0200

    Added current git commit to footer

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,5 +1,13 @@
 	<footer id="footer">
-		<p>&copy; <?= date('Y') ?> mm</p>
+		<p>
+			&copy; <?= date('Y') ?> mm -
+			<a href="<?= REPO_URL ?>commit/<?= urlencode(BUILD_HASH) ?>"
+				target="_blank"
+				rel="noopener noreferrer">
+				<?= htmlspecialchars(BUILD_HASH) ?>
+			</a>
+		</p>
 	</footer>
-</body>
-</html>
+	</body>
+
+	</html>
