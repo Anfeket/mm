abcdef02cf7b2d83b1383abb3bd88e42adfa12ef
anfeket <anfeket@gmail.com>
2025-10-14 22:07:52 +0200

    Styled footer

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,13 +1,13 @@
-	<footer id="footer">
-		<p>
-			&copy; <?= date('Y') ?> mm -
-			<a href="<?= REPO_URL ?>commit/<?= urlencode(BUILD_HASH) ?>"
-				target="_blank"
-				rel="noopener noreferrer">
-				<?= htmlspecialchars(BUILD_HASH) ?>
-			</a>
-		</p>
-	</footer>
-	</body>
+<footer id="footer">
+	<p>
+		&copy; <?= date('Y') ?> mm -
+		<a href="<?= REPO_URL ?>commit/<?= urlencode(BUILD_HASH) ?>"
+			target="_blank"
+			rel="noopener noreferrer">
+			<?= htmlspecialchars(BUILD_HASH) ?>
+		</a>
+	</p>
+</footer>
+</body>
 
-	</html>
+</html>
