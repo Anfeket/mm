81051448d7c0d7c897a14d7072460476ffe6735f
anfeket <anfeket@gmail.com>
2025-10-19 18:51:38 +0200

    Reload css on new commit

--- views/layout/head.php
+++ views/layout/head.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="/css/styles.css">
+	<link rel="stylesheet" href="/css/styles.css?v=<?= BUILD_HASH ?? 'dev' ?>">
 	<title><?= $title ?? 'mm' ?></title>
 	<?php if (isset($post)): ?>
 		<meta property="og:site_name" content="mm">
