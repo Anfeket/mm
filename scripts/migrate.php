6bb4605d8f1061d81c107d937d25ab3b24b3ec08
anfeket <anfeket@gmail.com>
2025-10-22 09:49:58 +0200

    Improved migrations to support .php files

--- scripts/migrate.php
+++ scripts/migrate.php
@@ -2,15 +2,22 @@
 require __DIR__ . '/../config/config.php';
 
 $applied = $pdo->query("SELECT name FROM migrations")->fetchAll(PDO::FETCH_COLUMN);
-$files = glob(__DIR__ . '/../migrations/*.sql');
+$files = glob(__DIR__ . '/../migrations/*.{sql,php}', GLOB_BRACE);
 sort($files);
 
 foreach ($files as $file) {
-    $name = basename($file);
-    if (in_array($name, $applied)) continue;
-    echo "Applying $name...\n";
-    $sql = file_get_contents($file);
-    $pdo->exec($sql);
-    $pdo->prepare("INSERT INTO migrations (name) VALUES (?)")->execute([$name]);
-    echo "→ Done\n";
+	$name = basename($file);
+	if (in_array($name, $applied)) continue;
+
+	echo "Applying $name...\n";
+
+	if (str_ends_with($file, '.sql')) {
+		$sql = file_get_contents($file);
+		$pdo->exec($sql);
+	} elseif (str_ends_with($file, '.php')) {
+		include $file;
+	}
+
+	$pdo->prepare("INSERT INTO migrations (name) VALUES (?)")->execute([$name]);
+	echo "→ Done\n";
 }
