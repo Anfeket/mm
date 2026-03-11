e736df068f80566e5e542a6808153a01e3ca0162
Branislav Farkaš <n5ye.yt@gmail.com>
2026-03-11 10:08:06 +0100

    Updated media.blade.php with new content.

--- resources/views/components/post/media.blade.php
+++ resources/views/components/post/media.blade.php
@@ -1,23 +1,26 @@
 @props(['post'])
 
-<section class="post-media-container">
+<figure class="post-media-container">
     @if ($post->isImage())
         <img
             src="{{ asset('uploads/' . $post->file_path) }}"
             alt="Post #{{ $post->id }}"
             @if ($post->width) width="{{ $post->width }}" @endif
             @if ($post->height) height="{{ $post->height }}" @endif
+            loading="lazy"
             class="post-media"
-        \>
+        >
     @elseif ($post->isVideo())
         <video
             controls
+            playsinline
+            @if ($post->thumb_path) poster="{{ asset('uploads/' . $post->thumb_path) }}" @endif
             @if ($post->width) width="{{ $post->width }}" @endif
             @if ($post->height) height="{{ $post->height }}" @endif
             class="post-media"
         >
-            <source src="{{ asset('uploads/' . $post->file_path) }}" type="video/mp4">
+            <source src="{{ asset('uploads/' . $post->file_path) }}" type="{{ $post->mime_type }}">
             Your browser does not support the video tag.
         </video>
     @endif
-</section>
+</figure>
