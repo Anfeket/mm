e6f7b6f7802d42e8f34e5035900dfd0162d18ad4
anfeket <n5ye.yt@gmail.com>
2026-03-18 16:55:44 +0100

    feat: Upgrade to Laravel 13, add Boost

--- config/cache.php
+++ config/cache.php
@@ -114,4 +114,17 @@ return [
 
     'prefix' => env('CACHE_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-cache-'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Serializable Classes
+    |--------------------------------------------------------------------------
+    |
+    | This option controls which classes may be unserialized from the cache.
+    | By default, no classes are allowed. If you store PHP objects in the
+    | cache, you should list them here for explicit unserialization.
+    |
+    */
+
+    'serializable_classes' => false,
+
 ];
