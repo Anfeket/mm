33d61461a4b86c8e30097a946a845c7f0cb48e74
anfeket <n5ye.yt@gmail.com>
2026-03-24 23:41:01 +0100

    fix(sitemap): render static sitemap URLs as strings

--- resources/views/sitemap/static.blade.php
+++ resources/views/sitemap/static.blade.php
@@ -3,10 +3,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 @foreach ($urls as $url)
     <url>
-        <loc>{{ $url['loc'] }}</loc>
-        @if (isset($url['lastmod']))
-            <lastmod>{{ $url['lastmod'] }}</lastmod>
-        @endif
+        <loc>{{ $url }}</loc>
     </url>
 @endforeach
 </urlset>
