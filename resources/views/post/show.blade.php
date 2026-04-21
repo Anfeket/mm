d0dfa19cf5b38d1f448b653f81d39908e9ff90c4
anfeket <n5ye.yt@gmail.com>
2026-04-21 15:00:25 +0200

    refactor(view): improve post description fallback logic to include tags and metadata in post show view

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -9,7 +9,7 @@
     </x-slot:sidebar>
 
     <x-slot:description>
-        {{ $post->description ? Str::limit($post->description, 160) : Str::limit(($post->tags->isNotEmpty() ? $post->tags->pluck('name')->implode(', ') : "Post #{$post->id} uploaded by {$post->author->username} on {$post->created_at->format('Y-m-d')}"), 160) }}
+        {{ Str::limit($post->description ? $post->description : "Post #{$post->id} uploaded by {$post->author->username} on {$post->created_at->format('Y-m-d')}" . ($post->tags->isNotEmpty() ? ' \u00b7 ' . $post->tags->pluck('name')->implode(', ') : ''), 160) }}
     </x-slot:description>
 
     <x-slot:jsonLd>{!! json_encode($jsonLd, JSON_UNESCAPED_SLASHES) !!}</x-slot:jsonLd>
