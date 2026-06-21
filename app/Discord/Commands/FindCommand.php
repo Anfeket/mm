4a784235c7cbaeca2d12b6c1b8dae6178f852df4
anfeket <n5ye.yt@gmail.com>
2026-06-21 15:11:54 +0200

    feat: add search by tags to discord command

--- app/Discord/Commands/FindCommand.php
+++ app/Discord/Commands/FindCommand.php
@@ -7,6 +7,7 @@ use App\Discord\Embed;
 use App\Discord\Interaction;
 use App\Discord\InteractionResponse;
 use App\Models\Post;
+use App\Services\TagService;
 
 class FindCommand implements DiscordCommand
 {
@@ -29,6 +30,19 @@ class FindCommand implements DiscordCommand
                         ],
                     ],
                 ],
+                [
+                    'type' => 1,
+                    'name' => 'tags',
+                    'description' => 'Find a post by tags',
+                    'options' => [
+                        [
+                            'type' => 3,
+                            'name' => 'tags',
+                            'description' => 'Tags to search for',
+                            'required' => true,
+                        ],
+                    ],
+                ]
             ]
         ];
     }
@@ -37,6 +51,7 @@ class FindCommand implements DiscordCommand
     {
         return match ($interaction->subcommand()) {
             'id' => $this->findById($interaction),
+            'tags' => $this->findByTags($interaction, app(TagService::class)),
             default => InteractionResponse::message()->content('Unknown subcommand.')->ephemeral(),
         };
     }
@@ -53,6 +68,57 @@ class FindCommand implements DiscordCommand
         }
 
         // Discord video bypass
+        if ($post->isVideo()) {
+            return InteractionResponse::message()
+                ->content(route('posts.show', $post));
+        }
+
+        $embed = (new Embed)
+            ->title("Post #{$post->id}")
+            ->url(route('posts.show', $post))
+            ->footer("Uploaded by {$post->author->username}")
+            ->timestamp($post->created_at);
+
+        if ($post->thumb_path && $post->isImage()) {
+            $embed->image(asset('uploads/' . $post->thumb_path));
+        }
+
+        if ($post->description) {
+            $embed->description($post->description);
+        }
+
+        return InteractionResponse::message()->embed($embed);
+    }
+
+    private function findByTags(Interaction $interaction, TagService $tagService): InteractionResponse
+    {
+        $tags = $tagService->parseSearchInput($interaction->subOption('tags'));
+
+        $query = Post::query()->where('is_listed', true);
+
+        foreach ($tags['include'] as $tag) {
+            $query->whereHas(
+                'tags',
+                fn($q) => $q
+                    ->where('name', $tag['name'])
+                    ->where('category', $tag['category'])
+            );
+        }
+        foreach ($tags['exclude'] as $tag) {
+            $query->whereDoesntHave(
+                'tags',
+                fn($q) => $q
+                    ->where('name', $tag['name'])
+                    ->where('category', $tag['category'])
+            );
+        }
+
+        $post = $query->with('author')->first();
+
+        if (!$post) {
+            return InteractionResponse::message()->content('No posts found with the given tags.')->ephemeral();
+        }
+
         if ($post->isVideo()) {
             return InteractionResponse::message()
                 ->content(route('posts.show', $post));
